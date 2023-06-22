d8374512b918a365857c5c6c134d5f496cc14332
mbongoelvis <mbongombongo4@gmail.com>
2023-06-22 16:53:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggoling = () =>{
+    let circle = document.querySelector(".circle")
+    let body = document.querySelector(".body")
+    circle.classList.toggle("toggler")
+    body.classList.toggle("colors")
+}
